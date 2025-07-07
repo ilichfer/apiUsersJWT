9308571a38b22f63c893f15cd37516b47de4162c
Ilich valbuena <ilichferv@hotmail.com>
2025-07-06 19:48:13 -0500

    add repository

--- src/main/java/com/smartjob/repository/PhoneRepository.java
+++ src/main/java/com/smartjob/repository/PhoneRepository.java
@@ -1,7 +1,6 @@
 package com.smartjob.repository;
 
 import com.smartjob.model.Phone;
-import com.smartjob.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- src/main/java/com/smartjob/repository/SessionRepository.java
+++ src/main/java/com/smartjob/repository/SessionRepository.java
@@ -1,6 +1,5 @@
 package com.smartjob.repository;
 
-import com.smartjob.model.User;
 import com.smartjob.model.UserSession;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
